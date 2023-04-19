c7c17336bd3f3271c46d78b408bbe3b251146622
ptan01 <ptanjim01@gmail.com>
2023-04-19 12:59:02 +0600

    right navigation add

--- src/Pages/Shared/rightNav/RightNav.jsx
+++ src/Pages/Shared/rightNav/RightNav.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub } from 'react-icons/fa';
+import QZone from '../QZone/QZone';
+import bg from '../../../assets/bg.png'
+import './rightNav.css'
 
 const RightNav = () => {
     return (
         <div>
-            <h2>Right side nav </h2>
+            <h4>Login with</h4>
+            <Button className='my-3' style={{ width: '100%' }} variant="outline-primary"> <FaGoogle></FaGoogle>Login with Google</Button>
+            <Button style={{ width: '100%' }} variant="outline-secondary"> <FaGithub></FaGithub>Login with Github</Button>
+            <div>
+                <h4 className='my-3'>Find Us On</h4>
+                <ListGroup>
+                    <ListGroup.Item>Facebook</ListGroup.Item>
+                    <ListGroup.Item>Twitter</ListGroup.Item>
+                    <ListGroup.Item>Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div>
+                <div className='bg-img'>
+                <h2>Create an Amazing Newspaper</h2>
+                <p>Discover thousands of options, easy to customize layouts, one-click to import demo and much more. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam alias, sequi voluptatem molestias laboriosam soluta quasi tempore nesciunt recusandae nobis neque maiores quae deleniti quisquam pariatur eveniet necessitatibus repellendus dolorum!</p>
+                <Button variant='danger'>Learn More</Button>
+                </div>
+            </div>
         </div>
     );
 };
