578c3171a96dcd729ec346060178d2daa15e8234
ptan01 <ptanjim01@gmail.com>
2023-04-19 12:05:38 +0600

    navbar added

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import logo from '../../../assets/logo.png'
-
+import moment from 'moment';
+import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
-    return (
-        <div>
-          <div className='text-center'>
-          <img src={logo} alt="" />
-            <p>Journalism Without Fear or Favour</p>
-            <h5>Sunday, November 27, 2022</h5>
-          </div>
+  return (
+    <div>
+      <div className='text-center'>
+        <img src={logo} alt="" />
+        <p>Journalism Without Fear or Favour</p>
+        <h5>{moment().format("dddd, MMMM D, YYYY")}</h5>
+      </div>
+      <Container>
+        <div className='d-flex'>
+          <Button variant="danger">Latest</Button>
+          <Marquee pauseOnHover='boolean' >
+            I can be a React component, multiple React components, or just some text. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quas totam officiis perferendis voluptatum nobis iusto maiores, temporibus exercitationem dolores magni. Rem minus necessitatibus voluptate quasi esse doloremque amet atque.
+          </Marquee>
         </div>
-    );
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+          <Container>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mx-auto">
+                <Nav.Link href="#features">Home</Nav.Link>
+                <Nav.Link href="#pricing">About</Nav.Link>
+                <Nav.Link href="#pricing">Career</Nav.Link>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#deets">Profile</Nav.Link>
+                <Button variant="dark">Logout</Button>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Container>
+    </div>
+  );
 };
 
 export default Header;
